2d05674f6ae2702339ece2babe6149719c73c2aa
Filip Kula <63972969+fkula5@users.noreply.github.com>
2024-10-16 09:40:41 +0000

    feat: 2.1

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
       .then((array) => {
         answer.innerHTML = "";
         array.forEach(function (item, index) {
-          answer.innerHTML += `<div><h2>${index} - ${item.title} </h2>
+          answer.innerHTML += `<div class="entry"><h2>${index} - ${item.title} </h2>
           <p>${item.body}</p></div>`;
         });
       });
@@ -33,7 +33,7 @@
       .then((array) => {
         setTimeout(() => {
           answer.innerHTML = "<hr>";
-          answer.innerHTML += `<div><h2> ${array.title} </h2>
+          answer.innerHTML += `<div class="entry"><h2> ${array.title} </h2>
         <p>${array.body}</p></div>`;
         }, 5000);
       });
